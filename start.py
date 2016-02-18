fc29b1156d6d03636319dc28269b99b701adbd94
garzoc <ost@airmail.cc>
2016-02-18 17:39:57 +0000

    doin some test

--- start.py
+++ start.py
@@ -2,5 +2,3 @@ from subprocess import call
 call('git add *',shell=True)
 call('git commit ',shell=True)
 call('git push origin master',shell=True)
-call('garzoc',shell=True)
-call('ostcake6',shell=True)
