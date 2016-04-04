585d327692426b1f0b7292375f0938b3c689b1ff
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-04-04 13:27:46 +0200

    Added setBluetoothData
    Added method to write data to bluetooth

--- BtConnection.java
+++ BtConnection.java
@@ -5,11 +5,13 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.*;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketException;
 import java.util.Set;
@@ -18,17 +20,20 @@ import java.util.UUID;
 
 /**
  * This Class handles the bluetooth connection between Arduino and Android
- * Author by Emanuel Mellblom
+ * @uthor Emanuel Mellblom
  */
-public class BtConnection extends Activity{
+
+public class BtConnection extends MainActivity{
 
     private final int REQUEST_ENABLE_BT = 12;
-    //public static TextView out;
     public static BluetoothAdapter adapter;
     static BluetoothDevice arduinoDevice;
     public static BluetoothSocket btSocket;
     static OutputStream btOutputStream;
     public static TextView out;
+
+    //New InputStream
+    static InputStream btInputStream;
 
 
 
@@ -48,61 +53,54 @@ public class BtConnection extends Activity{
         }
         return state;
     }
-/*
+
     public static void setBluetoothData() {
-        out = (TextView) findViewById(R.id.textView);
         // Getting the Bluetooth adapter and print out its name and adress
         adapter = BluetoothAdapter.getDefaultAdapter();
-        out.append("\nAdapter: " + adapter.toString() + "\n\nName: "
-                + adapter.getName() + "\nAddress: " + adapter.getAddress());
 
         // Check for Bluetooth support
         // Emulator doesn't support Bluetooth and will return null
         if (adapter == null) {
-            Toast.makeText(this, "Bluetooth NOT supported. Aborting.", Toast.LENGTH_LONG).show();
+           // AlertBoxes.turnOnBluetooth();
         }
         if (!adapter.isEnabled()) {
-            Intent enableBluetooth = new
-                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            MainActivity.startActivityForResult(enableBluetooth, 0);
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //AlertBoxes.turnOnBluetooth();
+
         }
         Set<BluetoothDevice> devices = adapter.getBondedDevices();
-        //  Set pairedDevices = adapter.getBondedDevices();
+         Set pairedDevices = adapter.getBondedDevices();
         if (devices.size() > 0) {
             for (BluetoothDevice device : devices) {
-                out.append("\nDevices Pared:");
-                out.append("\nFound device: " + device.getName() + " Add: "
-                        + device.getAddress());
                 if (device.getName().equals("Group 13")) {
                     //Device name = Group 13
-
                     arduinoDevice = device;
                     break;
                 }
             }
         }
         try {
-            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-            //Standard SerialPortService ID
+            System.out.println("getting here step 1");
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+            System.out.println("getting here step 2");
             btSocket = arduinoDevice.createRfcommSocketToServiceRecord(uuid);
+            System.out.println("getting here step 3");
             btSocket.connect();
+            System.out.println("getting here step 4");
             btOutputStream = btSocket.getOutputStream();
-            // btInputStream = btSocket.getInputStream();
+            System.out.println("getting here step 5");
+
+            //New InputStream
+            btInputStream = btSocket.getInputStream();
+
         } catch (SocketException so) {
             System.out.println("Socket failure");
+            /**
+             * This launches an alert box if bluetooth is disconnected.
+             */
+            System.out.println("socket connection faild");
         } catch (IOException io) {
-        }
-    }*/
-
-    public static void sendMsg(char msg){
-        char sendMsg = msg; //String msg = "f";
-        try {
-            btOutputStream.write(sendMsg); //msg.getBytes();
-            btOutputStream.flush();
-            System.out.println("Sending data");
-            System.out.println(btSocket.getOutputStream().toString());
-        } catch (IOException io) {
-            System.out.println("IO error writing to serial");
+            System.out.println("This is an error");
         }
     }
     }
