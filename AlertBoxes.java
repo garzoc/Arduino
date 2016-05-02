b44392a3597c10339d8e15380fdf19e549b674de
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-05-02 13:08:09 +0200

    Added files via upload

--- AlertBoxes.java
+++ AlertBoxes.java
@@ -3,24 +3,22 @@ package com.example.system.myapplication;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.view.View;
-import android.widget.Button;
 
 /**
- * Created by System on 2016-03-30.
+ * @author Emanuel Mellblom
+ * Class for making different alert boxes.
  */
 public class AlertBoxes extends MainActivity{
 
+    //Display an error message if the bluetooth is disconnected
     public static void bluetoothAlert(Context context) {
-
         new AlertDialog.Builder(context)
                 .setTitle("Bluetooth disconnected")
-                .setMessage("Do you want to reconnect?")
+                .setMessage("Do you want to connect?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         BtConnection.blueTooth();
                         BtConnection.setBluetoothData();
-                        //MainActivity.setBluetoothData();
                     }
                 })
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -32,6 +30,7 @@ public class AlertBoxes extends MainActivity{
                 .show();
     }
 
+    //Display error message if bluetooth is not activated on the device
     public static void turnOnBluetooth(Context context){
         AlertDialog turnOnBluetooth = new AlertDialog.Builder(context).create();
         turnOnBluetooth.setTitle("No Bluetooth");
@@ -45,20 +44,21 @@ public class AlertBoxes extends MainActivity{
         turnOnBluetooth.show();
     }
 
+    //New Display message when an obstacle is detected
     public static void obstacleDetected(Context context){
         AlertDialog turnOnBluetooth = new AlertDialog.Builder(context).create();
         turnOnBluetooth.setTitle("Obstacle detected");
-        turnOnBluetooth.setMessage("The car detected an obstacle in front of the car");
+        turnOnBluetooth.setMessage("The car detected an obstacle in front of the car be careful");
         turnOnBluetooth.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
+                        JoystickControl.obstacleAlertisShown = false;
                     }
                 });
         turnOnBluetooth.show();
     }
-
-    }
+}
 
 
 
