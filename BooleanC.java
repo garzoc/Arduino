0a4f04da49ef9916355ef54b07b283ac0496c6cf
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-05-14 10:15:06 +0200

    added a few extra variables

--- BooleanC.java
+++ BooleanC.java
@@ -1,15 +1,24 @@
 package com.example.system.myapplication;
 
-    /**
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+
+/**
      * Create boolean flags for other classes
      */
     public class BooleanC {
         private boolean bool;
         private int integer;
+        private Drawable drawable;
+        private Bitmap bitmap;
+
 
         public BooleanC(boolean bool){
             this.bool = bool;
             this.integer = 0;
+            this.drawable = null;
+            this.bitmap = null;
         }
 
         public void setBool(boolean bool){
@@ -26,6 +35,22 @@ package com.example.system.myapplication;
 
         public int getInt(){
             return this.integer;
+        }
+
+        public void setDrawable(Drawable drawable){
+            this.drawable = drawable;
+        }
+
+        public Drawable getDrawable(){
+            return this.drawable;
+        }
+
+        public Bitmap getBitmap(){
+            return this.bitmap;
+        }
+
+        public void setBitmap(Bitmap bitmap){
+            this.bitmap = bitmap;
         }
     }
 
